3967caf49205d16c053d723acb599ad1d0fd1867
wangshihan <258200029@qq.com>
2017-02-28 15:21:07 +0800

    change w01

--- w_01_bn_net.py
+++ w_01_bn_net.py
@@ -12,11 +12,35 @@ N_LAYERS = 2
 ACTIVATION = tf.nn.relu
 N_HIDDEN_UNITS = 128
 
-def built_net(xs, ys, norm):
+def built_net(xs, ys, bn):
     def add_layer(inputs, in_size, out_size, acfun=None):
         w = tf.Variable(tf.random_normal([in_size, out_size], mean=0., stddev=0.1))
         b = tf.Variable(tf.zeros([1, out_size]) + 0.1)
         wx_plus_b = tf.matmul(inputs, w) + b
+
+        if bn:
+            fc_mean, fc_var = tf.nn.moments(
+                    wx_plus_b,
+                    axes=[0], # the dimension you wanna normalize, here [0] for batch
+                            # for image, you wanna do [0, 1, 2] for [batch, height, width] but not channel
+            )
+            scale = tf.Variable(tf.ones([out_size]))
+            shift = tf.Variable(tf.zeros([out_size]))
+            epsilon = 0.001
+            
+            # apply moving average for mean and var when train on batch
+            ema = tf.train.ExponentialMovingAverage(decay=0.5)
+            def mean_var_with_update():
+                ema_apply_op = ema.apply([fc_mean, fc_var])
+                with tf.control_dependencies([ema_apply_op]):
+                    return tf.identity(fc_mean), tf.identity(fc_var)
+            mean, var = mean_var_with_update()
+
+            wx_plus_b = tf.nn.batch_normalization(wx_plus_b, mean, var, shift, scale, epsilon)
+            # similar with this two steps:
+            # Wx_plus_b = (Wx_plus_b - fc_mean) / tf.sqrt(fc_var + 0.001)
+            # Wx_plus_b = Wx_plus_b * scale + shift
+
         if acfun is None:
             outputs = wx_plus_b
         else:
@@ -59,8 +83,8 @@ def main():
     X = tf.placeholder('float', [None, 784])
     Y = tf.placeholder('float', [None, 10])
     
-    norm = False
-    train_op, cost, layers_inputs = built_net(X, Y, norm)
+    bn = True
+    train_op, cost, layers_inputs = built_net(X, Y, bn)
     
     py_x = layers_inputs[-1] # output
     predict_op = tf.argmax(py_x, 1)
