dff2498a9f7e215e9dd91088ad3d6ecd9cdbdb23
wangshihan <258200029@qq.com>
2017-03-01 11:48:03 +0800

    add 05

--- 05_conv_net.py
+++ 05_conv_net.py
@@ -28,7 +28,7 @@ def model(X, w, w2, w3, w4, w_o, p_keep_conv):
     
     l3a = tf.nn.relu(tf.nn.conv2d(l2, w3,     # l3a shape=(?, 7, 7, 128)
                         strides=[1,1,1,1], padding='SAME'))
-    l3 = tf.nn.max_pool(l2a, ksize=[1,2,2,1],   # l3 shape = (?, 4, 4, 128)
+    l3 = tf.nn.max_pool(l3a, ksize=[1,2,2,1],   # l3 shape = (?, 4, 4, 128)
                         strides=[1,2,2,1], padding='SAME')
     l3 = tf.reshape(l3, [-1, w4.get_shape().as_list()[0]])    # reshape to (?, 2048)
     l3 = tf.nn.dropout(l3, p_keep_conv)
